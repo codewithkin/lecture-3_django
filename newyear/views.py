5e8c7af96aea3bf413d0c857fc597411e85b7d8a
KIn Leon Zzinzombe <kinzinzombe07@gmail.com>
2023-07-07 16:10:50 +0200

    Add functionality

--- newyear/views.py
+++ newyear/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
+def index(request):
+    return render(request, 'newyear/index.html',{
+        date: datetime.datetime.now()
+    })
